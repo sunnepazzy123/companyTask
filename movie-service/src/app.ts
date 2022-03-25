a3fd629497583f70f67baa656d2d4d9127a0dc1b
unknown <sunnepazzy123@gmail.com>
2022-03-25 19:40:50 +0100

    update readme

--- movie-service/src/app.ts
+++ movie-service/src/app.ts
@@ -14,12 +14,12 @@ app.use(express.json());
 app.use('/api', moviesRoutes);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs)); 
 //Global Error Handler
-app.use((err: IError, req: Request, res: Response, next: NextFunction)=>{
+app.use((err: IError, req: Request, res: Response, _next: NextFunction)=>{
 
-    if(err.statusCode === 400){
+    if(err.statusCode === 400 || err.statusCode != 404){
        return res.status(404).json({message: err.message, data: null, path: req.path, method: req.method});
     }
-    return res.status(404).json({message: err.message, data: null, path: req.path, method: req.method});
+    return res.status(500).json({message: err.message, data: null, path: req.path, method: req.method});
 });
 
 export { app }
